82e79d75e887f91c3b62615038828db42ada8ef4
Taka Okunishi <okunishitaka.com@gmail.com>
2016-02-05 18:30:02 +0900

    Add sort func

---
+++ test/sort_properties_test.js
@@ -0,0 +1,33 @@
+/**
+ * Test case for sortProperties.
+ * Runs with mocha.
+ */
+"use strict";
+
+const sortProperties = require('../lib/sort_properties.js'),
+    assert = require('assert');
+
+describe('sort-properties', () => {
+
+    before((done) => {
+        done();
+    });
+
+    after((done) => {
+        done();
+    });
+
+
+    it('Sort properties', (done) => {
+        let sorted = sortProperties({
+            foo: {
+                quz: 'quz',
+                bar: 'baz',
+            },
+            hoge: 2
+        });
+        assert.ok(sorted);
+        done();
+    });
+});
+
